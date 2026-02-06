b2c458d939c1ffc8f6a13efd6d618c346d0f1094
Rodion <rodion1900@gmail.com>
2026-02-06 15:34:23 +0200

    Simple Routing

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,12 +1,13 @@
-import {SwContext} from "../utils/context.ts";
-import {useContext} from "react";
+
 interface Props {
     itemTitle: string
 }
 const NavItem = ({itemTitle}:Props) => {
-    const {changePage} = useContext(SwContext)
     return (
-        <li onClick={() => changePage(itemTitle)} className={`bg-danger rounded-[5px] border-black border-2 px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>{itemTitle}</li>
+
+        <li className={`bg-danger rounded-[5px] border-black border-2 px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>
+        <a href={`/${itemTitle}`}>{itemTitle} </a>
+        </li>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 // import { StrictMode } from 'react' // temporarily removed
+//import {
+//     createBrowserRouter,
+//     RouteProvider,
+// } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+   <BrowserRouter>
+       <App />
+   </BrowserRouter>
 )
